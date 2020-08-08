fc25011961210af352fcbe24043ef7bfa36d4ffa
jaygatsby578 <66869535+jaygatsby578@users.noreply.github.com>
2020-08-09 03:46:53 +0900

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,4 +1,5 @@
 const images =  ["a1.jpeg", "a2.jpeg", "a3.jpeg"];
+var myAudio = new Audio('AnyConv.com__ミスリル鉱山で迷子.ogg');
 
 let count = 0;
 const slideimage =()=>{
@@ -14,9 +15,11 @@ let slideid = 0;
 const startstop = () =>{
   if(slideid === 0){//開始ボタンを押した時の処理内容
     slideid = setInterval(slideimage, 1000);
+    myAudio.play();
   　}else{ //停止ボタンを押した時の処理内容
     　clearInterval(slideid);
     　slideid = 0;
+     myAudio.pause();
   　}
 }
 document.getElementById('startstopbtn').onclick = startstop;
