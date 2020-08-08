49a2fa4eae4de272209d3bca44840a70ac2c59f5
Ubuntu <ubuntu@ip-172-31-39-218.us-east-2.compute.internal>
2020-08-08 18:19:54 +0000

    first

---
+++ slider.js
@@ -0,0 +1,22 @@
+const images =  ["a1.jpeg", "a2.jpeg", "a3.jpeg"];
+
+let count = 0;
+const slideimage =()=>{
+  if( count >= images.length){
+    count = 0;
+  }else{
+    document.getElementById('slideshow').src = images[count];
+    count++;
+  }
+}
+
+let slideid = 0;
+const startstop = () =>{
+  if(slideid === 0){//開始ボタンを押した時の処理内容
+    slideid = setInterval(slideimage, 5000);
+  　}else{ //停止ボタンを押した時の処理内容
+    　clearInterval(slideid);
+    　slideid = 0;
+  　}
+}
+document.getElementById('startstopbtn').onclick = startstop;
